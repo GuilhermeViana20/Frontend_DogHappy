6f2bed42c6431d8045fd348136336c7423b86cc7
GuilhermeViana25 <guilhermeviana347@gmail.com>
2021-11-05 00:27:07 -0300

    Projeto iniciado - primeiro commit

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+	<div id="Home" class="fixed-sn black-skin">
+		<h1>Home</h1>
+		
+	</div>
+</template>
+
+<script>
+
+export default {
+	name: 'Home',
+	components: {
+	}
+}
+</script>
+
+<style>
+
+</style>
